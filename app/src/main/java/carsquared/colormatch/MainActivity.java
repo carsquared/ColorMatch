a206f1667835b63476209ba3aadbbd3b17dac8ee
CarSquared <carsquareddesign@gmail.com>
2018-08-17 19:23:42 -0500

    fixed options dialog

--- app/src/main/java/carsquared/colormatch/MainActivity.java
+++ app/src/main/java/carsquared/colormatch/MainActivity.java
@@ -1,9 +1,12 @@
 package carsquared.colormatch;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -13,8 +16,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -52,15 +57,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
-    // Client used to sign in with Google APIs
-    private GoogleSignInClient mGoogleSignInClient;
-
-
-
     // progress dialog we display while we're loading state from the cloud
     ProgressDialog mLoadingDialog = null;
 
-    GoogleSignInOptions gso;
 
 
     @Override
@@ -81,7 +80,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         soundPrefs = getSharedPreferences(soundData, MODE_PRIVATE);
         sound = soundPrefs.getInt(soundInt, defaultSoundInt);
         textviewcoinnum = findViewById(R.id.textviewcoinnum);
-        // textviewcoinnum.setText(String.valueOf(coinamt));
+       // textviewcoinnum.setText(String.valueOf());
     }
 
     private void showAchievements() {
@@ -134,86 +133,78 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 go.setClass(getApplicationContext(),options_dialog_Activity.class);
                 startActivity(go);
 
-                //    sound = soundPrefs.getInt(soundInt, defaultSoundInt);
-                //               soundEditor = soundPrefs.edit();
-                //    final Dialog dia = new Dialog(context);
-                //             dia.setContentView(R.layout.options);
-                //           dia.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                //         dia.setCanceledOnTouchOutside(false);
-                //       dia.show();
+                sound = soundPrefs.getInt(soundInt, defaultSoundInt);
+                soundEditor = soundPrefs.edit();
+                final Dialog dia = new Dialog(context);
+                dia.setContentView(R.layout.options_dialog);
+                dia.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dia.setCanceledOnTouchOutside(false);
+                dia.show();
 
-//                ImageButton closebut = dia.findViewById(R.id.closebut);
-                //              final ToggleButton soundbut = dia.findViewById(R.id.soundbut);
-                //            Button creditbut = dia.findViewById(R.id.creditbut);
-                //          awardsLink = dia.findViewById(R.id.awardsLink);
+               ImageButton closebut = dia.findViewById(R.id.closebut);
+               final ToggleButton soundbut = dia.findViewById(R.id.soundbut);
+               Button creditbut = dia.findViewById(R.id.creditbut);
+               awardsLink = dia.findViewById(R.id.awardsLink);
 
-//                awardsLink.setOnClickListener(new View.OnClickListener() {
-                //        @Override
-                //        public void onClick(View view) {
-                //      showAchievements();
+               awardsLink.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                showAchievements();
 
-                //                  }
-                //      });
-
-
-                //            llPlay = dia.findViewById(R.id.llPlay);
-
-                //          llPlay.setOnClickListener(new View.OnClickListener() {
-                //            @Override
-                //        public void onClick(View view) {
-                //        //   showLeaderboard();
+                }
+                });
 
 
-//                    }
-                //      });
+                llPlay = dia.findViewById(R.id.llPlay);
 
-                //              buttonSignOut = dia.findViewById(R.id.buttonSignOut);
-                //            buttonSignOut.setOnClickListener(new View.OnClickListener() {
-                //               @Override
-                //             public void onClick(View view) {
-                //               signOut();
+                llPlay.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+               // showLeaderboard();
 
-                //         }
-                //   });
+                }
+                });
 
-                //          closebut.setOnClickListener(new View.OnClickListener() {
-                //                @Override
-                //              public void onClick(View v) {
-                //                dia.dismiss();
-                //          }
-                //    });
 
-                //        creditbut.setOnClickListener(new View.OnClickListener() {
-                //      @Override
-                //    public void onClick(View v) {
-                //              go.setClass(getApplicationContext(),creditsactivity.class);
-                //            startActivity(go);
 
-                //      }
-                //  });
+                closebut.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                dia.dismiss();
+                }
+                });
 
-                //if (sound == 1){
-                //                   soundbut.setChecked(true);
-                //             } else if (sound == 0){
-                //               soundbut.setChecked(false);
-                //         }
-                //       if (soundbut.isChecked()){
-                //         soundEditor.putInt(soundInt, 1).apply();
-                //   }
+                creditbut.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                go.setClass(getApplicationContext(),creditsactivity.class);
+                startActivity(go);
 
-                //          soundbut.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                //            @Override
-                //          public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                }
+                });
 
-                //                  if (isChecked){
-                //                    soundEditor.putInt(soundInt, 1).apply();
-                //              } else {
-                //                soundEditor.putInt(soundInt, 0).apply();
-                //          }
+                if (sound == 1){
+                soundbut.setChecked(true);
+                } else if (sound == 0){
+                soundbut.setChecked(false);
+                }
+                if (soundbut.isChecked()){
+                soundEditor.putInt(soundInt, 1).apply();
+                }
 
-                //    }
-                //            });
-                //          break;
+                soundbut.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked){
+                soundEditor.putInt(soundInt, 1).apply();
+                } else {
+                soundEditor.putInt(soundInt, 0).apply();
+                }
+
+                }
+                });
+                break;
 
         }
     }
@@ -231,7 +222,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Since the state of the signed in user can change when the activity is not active
         // it is recommended to try and sign in silently from when the app resumes.
-        // signInSilently();
+       // signInSilently();
     }
 
 
